5f62a46583ab30ae74fbd7fbf04220a951b39e3a
Fyks <kotanic7@gmail.com>
2017-06-17 11:47:01 +0300

    templates

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, world!"
+    user = {'nickname': 'kotya'}
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+@app.route('/nya')
+def nya():
+    return 'Nyaaa~'
