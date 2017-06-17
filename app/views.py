5d428d7cf254a0e44e555e191426bde2d01490b7
Fyks <kotanic7@gmail.com>
2017-06-17 12:26:53 +0300

    views, templates, comments

--- app/views.py
+++ app/views.py
@@ -6,9 +6,21 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'kotya'}
+    posts = [
+        {
+            'author': {'nickname': 'Dog'},
+            'body': 'Woof! Woof!'
+        },
+        {
+            'author': {'nickname': 'Bird'},
+            'body': 'Tweet, motherfucker! Tweet!'
+        }
+    ]
     return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
+
 
 @app.route('/nya')
 def nya():
