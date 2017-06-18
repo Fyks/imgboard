60724b2f1b3c4e06d1a419e596d9ff6bb4611490
Fyks <kotanic7@gmail.com>
2017-06-18 15:29:31 +0300

    form "login", validators, redirect

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-from forms import LoginForm
+from app.form import LoginForm
 
 
 @app.route('/')
@@ -31,7 +31,13 @@ def nya():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = ()
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenId="' +
+              form.openid.data +
+              '", remember_me=' +
+              str(form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
                            form=form)
